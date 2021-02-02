8cb54f081aa85c4275013f00ee71c42414e8d235
jpwf <joaopedroweydt@gmail.com>
2021-02-02 20:19:49 -0300

    Pomodoro graph part version 0.1.0

--- Graph-part/graph-screen.py
+++ Graph-part/graph-screen.py
@@ -4,31 +4,45 @@ from time import sleep
 
 class screen:
     def __init__(self, master=None):
-        
         window.title("Pomodoro project")
+        self.color0 = "gray"
+        self.color1 = "green"
+        self.color2 = "orange"
+        self.color3 = "red"
+
+        self.container0 = Frame(master)
+        self.container0.pack()
 
         self.container1 = Frame(master)
+        self.container1["padx"] = 30
+        self.container1["pady"] = 4
         self.container1.pack()
 
         self.container2 = Frame(master)
+        self.container2["padx"] = 20 
+        self.container2["pady"] = 4
         self.container2.pack()
         
         self.container3 = Frame(master)
+        self.container3["padx"] = 20
+        self.container3["pady"] = 4
         self.container3.pack()
 
         
 
-        self.texto1 = Label(self.container1, text="Bem vindo ao Projeto pomodoro", bg="blue")
+        self.texto0 = Label(self.container0, text="Bem vindo ao Projeto pomodoro")
+        self.texto0["font"] = ("Arial", "10")
+        self.texto0.pack()
+
+        self.texto1 = Label(self.container1, text="Primeiro container", bg="gray", width=20)
         self.texto1["font"] = ("Arial", "10")
         self.texto1.pack()
-
         
-        
-        self.texto2 = Label(self.container2, text="Segundo container", bg="red")
+        self.texto2 = Label(self.container2, text="Segundo container", bg="gray",width=20)
         self.texto2["font"] = ("Arial", "10")
         self.texto2.pack()
 
-        self.texto3 = Label(self.container3, text="Terceiro container", bg="orange")
+        self.texto3 = Label(self.container3, text="Terceiro container", bg="gray",width=20)
         self.texto3["font"] = ("Arial", "10")
         self.texto3.pack()
 
@@ -37,20 +51,26 @@ class screen:
         self.teste["text"] = "Trocar cor"
         self.teste["font"] = ("Calibri", "9")
         self.teste["width"] = 10
-        self.teste["command"] = self.color
+        self.teste["command"] = self.c1
         self.teste.pack()
 
-    def color(self):
+    def c1(self):
         sleep(3)
-        self.texto1["background"] ="yellow"
-        self.teste["command"] = self.color2
-    def color2(self):
+        self.texto1["background"] = self.color1
+        self.teste["command"] = self.c2
+    def c2(self):
         sleep(3)
-        self.texto2["background"] = "purple"
-        self.teste["command"] = self.color3
-    def color3(self):
+        self.texto2["background"] = self.color2
+        self.teste["command"] = self.c3
+    def c3(self):
         sleep(3)
-        self.texto3["background"] = "red"
+        self.texto3["background"] = self.color3
+        self.teste["command"] = self.c0 
+    def c0(self):
+        sleep(1)
+        self.texto1["background"] = self.color0
+        self.texto2["background"] = self.color0
+        self.texto3["background"] = self.color0
 
 
         
