89d1d4908797b95f17402d7be0c166f5808292c2
jpwf <joaopedroweydt@gmail.com>
2021-02-15 00:27:49 -0300

    version 1.5.0

--- Graph-part/graph-screen.py
+++ Graph-part/graph-screen.py
@@ -1,78 +1,88 @@
 from Tkinter import *
 from time import sleep
-from pyfirmata import Arduino, util
+# from pyfirmata import Arduino, util
 
-board = Arduino('/dev/ttyUSB0')
-it = util.Iterator(board)
-it.start()
-board.digital[7].write(0)
+# board = Arduino('/dev/ttyUSB0')
+# it = util.Iterator(board)
+# it.start()
+# board.digital[7].write(0)
 
 
 
 class screen:
     def __init__(self, master=None):
         window.title("Pomodoro project")
-        self.font = ("Arial", "10")
+        self.font = ("Arial", "15")
         self.color1 = "green"
         self.color2 = "orange"
         self.color3 = "red"
 
         self.container0 = Frame(master)
         self.container0.pack()
+        self.container0["padx"] = 120
+        self.container0["pady"] = 120
 
         self.color0 = self.container0.cget("bg")
 
         self.container1 = Frame(master)
         self.container1["padx"] = 30
-        self.container1["pady"] = 4
+        self.container1["pady"] = 8
         self.container1.pack()
 
         self.container2 = Frame(master)
-        self.container2["padx"] = 20 
-        self.container2["pady"] = 4
+        self.container2["padx"] = 30 
+        self.container2["pady"] = 18
         self.container2.pack()
         
         self.container3 = Frame(master)
-        self.container3["padx"] = 20
-        self.container3["pady"] = 4
+        self.container3["padx"] = 30
+        self.container3["pady"] = 8
         self.container3.pack()
 
         self.container4 = Frame(master)
-        self.container4["padx"] = 20
-        self.container4["pady"] = 4
+        self.container4["padx"] = 30
+        self.container4["pady"] = 8
         self.container4.pack()
+
+        self.container5 = Frame(master)
+        self.container5["padx"] = 30
+        self.container5["pady"] = 8
+        self.container5.pack()
 
         
 
         self.texto0 = Label(self.container0, text="Bem vindo ao Projeto pomodoro")
-        self.texto0["font"] = self.font
+        self.texto0["font"] = ("Arial", "20")
         self.texto0.pack()
 
         self.texto1 = Label(self.container1)
-        self.texto1["width"] = 22
+        self.texto1["width"] = 35
+        self.texto1["height"] = 2
         self.texto1["font"] = self.font
         self.texto1.pack()
         
         self.texto2 = Label(self.container2)
-        self.texto2["width"] = 22
+        self.texto2["width"] = 35
+        self.texto2["height"] = 2
         self.texto2["font"] = self.font
         self.texto2.pack()
 
         self.texto3 = Label(self.container3)
-        self.texto3["width"] = 22
+        self.texto3["width"] = 35
+        self.texto3["height"] = 2
         self.texto3["font"] = self.font
         self.texto3.pack()
 
-        self.msg = Label(self.container4)
-        self.msg["width"] = 15
+        self.msg = Label(self.container5)
+        self.msg["width"] = 30
         self.msg["text"] = ""
         self.msg["font"] = self.font
         self.msg.pack()
 
-        self.action = Button(self.container3)
+        self.action = Button(self.container4)
         self.action["text"] = "Iniciar"
-        self.action["font"] = ("Calibri", "9")
-        self.action["width"] = 10
+        self.action["font"] = self.font
+        self.action["width"] = 30
         self.action["command"] = self.t1
         self.action.pack()
 
@@ -89,10 +99,10 @@ class screen:
         
 
     def timer1(self):
-        sleep(5400)
-        board.digital[7].write(1)
+        sleep(5)
+        # board.digital[7].write(1)
         sleep(0.08)
-        board.digital[7].write(0)
+        # board.digital[7].write(0)
         self.texto1["text"] = ""
         self.texto1["background"] = self.color0
         self.action["text"] = "Start timer"
@@ -102,10 +112,10 @@ class screen:
         self.texto2["text"] = "Its break time !!!"
         
     def timer2(self):
-        sleep(1200)
-        board.digital[7].write(1)
+        sleep(2)
+        # board.digital[7].write(1)
         sleep(0.08)
-        board.digital[7].write(0)
+        # board.digital[7].write(0)
         self.texto2["text"] = ""
         self.texto2["background"] = self.color0
         self.action["command"] = self.timer3
